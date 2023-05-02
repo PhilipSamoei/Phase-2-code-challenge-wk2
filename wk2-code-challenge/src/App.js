d56d4e26e608f40c400636c6a97197c11919620a
Philip Samoei <samphilkerich04@gmail.com>
2023-05-02 16:03:35 +0300

    complete code

--- wk2-code-challenge/src/App.js
+++ wk2-code-challenge/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BotsPage from "./components/bot/BotsPage";
+import './App.css' 
 
 function App() {
+  const [botCollection, setBotCollection] = React.useState([]) 
+  const [checkBotCollection, setCheckBotCollection] = React.useState(false) 
+
+  //fetching data from the server
+  React.useEffect(() => {
+    fetch("https://api.npoint.io/fc95b7d51ec3b28254bc/bots/")
+      .then(res => res.json())
+      .then(bots => {
+        
+        setBotCollection(bots)
+      })
+      
+  }, [checkBotCollection]) 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BotsPage 
+      
+      botCollection = {botCollection}
+      setBotCollection = {setBotCollection}
+      setCheckBotCollection = {setCheckBotCollection}
+      />
     </div>
   );
 }
